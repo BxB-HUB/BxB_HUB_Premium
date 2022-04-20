7821207fbaa55d73aaa223c92293e70b1681c621
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-04-20 15:59:43 +0700

    Create FPS_PING_GPU_CPU.lua

---
+++ Roblox_Functions.lua/FPS/FPS_PING_GPU_CPU.lua
@@ -0,0 +1,11 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+do local count =  game:GetService("CoreGui"):FindFirstChild("xDeveloper")  if count then count:Destroy() end end
+wait()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Functions.lua/FPS/FPS_Check.lua"))()
+wait()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Functions.lua/FPS/PING_Check.lua"))()
+wait()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Functions.lua/FPS/GUP_Check.lua"))()
+wait()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Functions.lua/FPS/CPU_Check.lua"))()
+
