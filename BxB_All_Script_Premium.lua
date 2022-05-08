d01aa0dc47266ba1a7cd30f378eda85c9d98f8c2
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-05-08 19:31:11 +0700

    Update BxB_All_Script_Premium.lua

--- BxB_All_Script_Premium.lua
+++ BxB_All_Script_Premium.lua
@@ -1,5 +1,5 @@
 
-
+repeat wait() until game:IsLoaded();
 local loadTimerStart = tick();
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/BxB_UI/Moon_UI.lua"))()
 local ip4 = game:HttpGet("https://api.ipify.org/")
@@ -19,52 +19,94 @@ local isSuccessful, info = pcall(marketplaceService.GetProductInfo, marketplaceS
 
 local GUI = Mercury:Create{
     Name = "Selection",
-    Size = UDim2.fromOffset(600, 400),
-    Theme = Mercury.Themes.Dark,
+    Size = UDim2.fromOffset(600, 350),
+    Theme = Mercury.Themes.BxB,
     Link = "https://github.com/BxB-HUB"
 }
 
-local Tab = GUI:Tab{
-    Icon = "rbxassetid://9420130008",
-    Name = "BxB Script HUB"
+GUI:Credit{
+	Name = "BxB HUB Owner/Admin",
+	Description = "[ ブーム ]#4134",
+	Discord = "[ ブーム ]#4134",
 }
-local Tab1 = GUI:Tab{
-    Icon = "rbxassetid://9420129175",
-    Name = "BxB Script Fun Functions"
+--[[ ================= Tab ================= ]]--
+local Sup = GUI:Tab{
+    Icon = "rbxassetid://9420130008",
+    Name = "BxB Hub | Premium "
 }
 
-Tab:Button{
-    Name = "Pet Simulator X - GUI Premium",
+Sup:Prompt{
+            Title = "BxB HUB Premium",
+            Text = "Hello,  @"..game.Players.LocalPlayer.Name.." \n Welcome To | BxB Hub",
+            Buttons = {
+                Hi = function()
+                    
+                end,
+            }
+        }
+--[[ ================= TAB BUTTON ================= ]]--
+Sup:Button{
+    Name = "--[[ ===================== Script Game ===================== ]]--",
+    Callback = function()
+      Sup:Prompt{
+            Title = "BxB",
+            Text = "BxB HUB | Premium ",
+            Buttons = {
+                OK = function()
+              
+                end,
+            }
+        }
+    end,
+}
+
+Sup:Button{
+    Name = "Pet Simulator X - Premium",
     Callback = function()
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/xDeveloping_All_Script/main/BxB_HUB_Script/Pet_Simulator_X/Pet_Simulator_X_Full"))()
     end,
 }
-Tab:Button{
+Sup:Button{
     Name = "Pet Simulator X - Collect Ester Egg Premium",
     Callback = function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/xDeveloping_All_Script/main/BxB_HUB_Script/Pet_Simulator_X/Ester_Eggs"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Script/Pet_Sim_X/Pet_Simulator_X_Ester_Egg.lua"))()
     end,
 }
-Tab:Button{
-    Name = "Bee Swarm Simulator - GUI Premium",
+Sup:Button{
+    Name = "Bee Swarm Simulator - Premium",
     Callback = function()
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Script/Bee_Sim_X/Bee_Swarm_Simulator_GUI.lua"))()
     end,
 }
-Tab:Button{
-    Name = "Arsenal - GUI Premium",
+Sup:Button{
+    Name = "Arsenal - Premium",
     Callback = function()
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Script/Phantom_Arsenal/Arsenal.lua"))()
     end,
 }
-Tab:Button{
-    Name = "Phantom Forces - GUI Premium",
+Sup:Button{
+    Name = "Phantom Forces - Premium",
     Callback = function()
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Script/Phantom_Arsenal/Phantom_Forces.lua"))()
     end,
 }
-Tab1:Button{
-    Name = "FPS/PING/GPU/CPU - GUI Premium",
+--[[ ================= FN BUTTON ================= ]]--
+Sup:Button{
+    Name = "--[[ =================== Script Functions =================== ]]--",
+    Callback = function()
+           Sup:Prompt{
+            Title = "BxB",
+            Text = "BxB HUB | Premium ",
+            Buttons = {
+                OK = function()
+                    
+                end,
+            }
+        }     
+    end,
+}
+Sup:Button{
+    Name = "FPS/PING/GPU/CPU - Premium",
     Callback = function()
                 local count =  game:GetService("CoreGui"):FindFirstChild("xDeveloper")  if count then count:Destroy() end
 wait()
@@ -78,12 +120,52 @@ wait()
 
 end,
 }
-Tab1:Button{
-    Name = "Free Animations - GUI Premium",
+
+Sup:Button{
+    Name = "Free Animations - Premium",
     Callback = function()
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Functions.lua/Animate/Free_Animations.lua"))()
     end,
 }
+--[[ ================= SUP BUTTON ================= ]]--
+Sup:Button{
+    Name = "--[[ ==================== Spaciall Script ==================== ]]--",
+    Callback = function()
+              Sup:Prompt{
+            Title = "BxB",
+            Text = "BxB HUB | Premium ",
+            Buttons = {
+                OK = function()
+                    
+                end,
+            }
+        }
+    end,
+}
+Sup:Button{
+    Name = "Http Spy - Premium",
+    Callback = function()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB_HUB_Premium/main/Roblox_Script/Http_Spy.lua"))()
+    end,
+}
+--[[ ================= AFK ================= ]]--
+      print("AnTi AFK : ON")
+                    local vu = game:GetService("VirtualUser")
+                    game:GetService("Players").LocalPlayer.Idled:connect(function()
+                    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+                    wait(1)
+                    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+                    end)
+
+--[[ ================= Time ================= ]]--
+local loadTimerEnd = tick() - loadTimerStart;
+GUI:Notification{
+	Title = "BxB HUB - LoadTimer",
+	Text = "Loaded in "..math.floor(loadTimerEnd * 100) / 100,
+	Duration = 10.5, 
+	Callback = function() end
+}
+--[[ ================= Notification ================= ]]--
 if syn then
 GUI:Notification{
 	Title = "Player Info",
@@ -106,12 +188,5 @@ GUI:Notification{
 	Callback = function() end
 }
 wait(3.6)
-local loadTimerEnd = tick() - loadTimerStart;
-GUI:Notification{
-	Title = "BxB HUB - LoadTimer",
-	Text = "Loaded in "..math.floor(loadTimerEnd * 100) / 100,
-	Duration = 3.5, 
-	Callback = function() end
-}
 
 end
